f57dc4e83ea34b3f5abcf1999065c2372fd90967
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-01-09 14:17:27 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -25,10 +25,12 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
-const memoryGame = new MemoryGame(cards);
+window.addEventListener('load', () => {
+  const memoryGame = new MemoryGame(cards);
+  let pickedCards = [];
 
-window.addEventListener('load', (event) => {
   let html = '';
+  memoryGame.shuffleCards(); 
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -38,14 +40,64 @@ window.addEventListener('load', (event) => {
     `;
   });
 
-  // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if (!card.classList.contains('turned') && !card.classList.contains('blocked')) {
+        card.classList.toggle('turned');
+        pickedCards.push(card);
+
+        if (pickedCards.length === 2) {
+          const [card1, card2] = pickedCards;
+          const card1Name = card1.dataset.cardName;
+          const card2Name = card2.dataset.cardName;
+
+          if (memoryGame.checkIfPair(card1Name, card2Name)) {
+            card1.classList.add('blocked');
+            card2.classList.add('blocked');
+          } else {
+            setTimeout(() => {
+              card1.classList.remove('turned');
+              card2.classList.remove('turned');
+            }, 1000);
+          }
+          pickedCards = [];
+          if (memoryGame.checkIfFinished()) {
+            alert('You won!!!');
+          }
+          updateScore();
+        }
+        updateScoreMarker();
+      }
     });
   });
+
+  function updateScore() {
+    memoryGame.pairsClicked++;
+  }
+
+  function updateScoreMarker() {
+    const pairsClickedElement = document.getElementById('pairs-clicked');
+    const pairsGuessedElement = document.getElementById('pairs-guessed');
+
+    pairsClickedElement.textContent = memoryGame.pairsClicked;
+    pairsGuessedElement.textContent = memoryGame.pairsGuessed;
+
+    const marker = document.getElementById('score-marker');
+    const totalPairs = cards.length / 2;
+    const markerPosition = (memoryGame.pairsGuessed / totalPairs) * 100;
+    marker.style.width = `${markerPosition}%`;
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
